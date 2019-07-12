d6f544ccb953ac1acc3778623133bf74a6f38779
Jerry <jerryanc6@gmail.com>
2019-07-12 15:22:12 -0500

    restserver usurio

--- server/config/config.js
+++ server/config/config.js
@@ -1,2 +1,19 @@
 // puerto
 process.env.PORT = process.env.PORT || 3000
+
+//entorno
+process.env.NODE_ENV = process.env.NODE_ENV || 'dev'
+
+
+//base de datos
+
+
+let urlDB;
+
+if (process.env.NODE_ENV === 'dev') {
+    urlDB = 'mongodb://localhost:27017/cafe';
+} else {
+    urlDB = 'mongodb+srv://jerryanc94:C@pricornio94@cluster0-iv3f9.mongodb.net/cafe';
+}
+
+process.env.URLDB = urlDB;
--- server/server.js
+++ server/server.js
@@ -1,11 +1,27 @@
-require('./config/config')
-const express = require('express');
-const app = express();
+require('./config/config');
 
-app.get('/', function(req, res) {
-    res.send('Hello World')
-})
+const express = require('express');
+const mongoose = require('mongoose');
+const cors = require('cors');
+
+const app = express();
+app.use(cors());
+
+const bodyParser = require('body-parser');
+
+// parse application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({ extended: false }))
+
+// parse application/json
+app.use(bodyParser.json())
+
+app.use(require('./routes/usuario'));
+
+mongoose.connect(process.env.URLDB, { useNewUrlParser: true, useCreateIndex: true }, (err, res) => {
+    if (err) throw err;
+    console.log('base de datos online');
+});
 
 app.listen(process.env.PORT, () => {
-    console.log('Escuchando puerto:', process.env.PORT);
-})
+    console.log('Escuchando puerto: ', process.env.PORT);
+});
